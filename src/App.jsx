c1ed32bacab6adbc1475c835d9f07fede21e40c2
Raúl Palma Kaschel <rpalmakaschel@gmail.com>
2023-10-07 11:34:43 -0300

    desafio react 2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Registro from './components/Registro';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="center-container">
+        <Registro />
+      </div>
+    </div>
+  );
+}
+
+export default App;
